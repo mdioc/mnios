a68238788eea47a384ba16f0095a95ce3a2ff226
mdioc <oss@znta.io>
2025-11-09 22:08:16 -0500

    switching base image

--- Containerfile
+++ Containerfile
@@ -1,6 +1,6 @@
 FROM scratch AS ctx
 COPY build_files /
-FROM ghcr.io/ublue-os/base-main:latest
+FROM ghcr.io/ublue-os/aurora-dx:latest
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
--- build_files/build.sh
+++ build_files/build.sh
@@ -4,35 +4,6 @@ set -ouex pipefail
 
 dnf5 install -y tmux niri sddm alacritty firefox dolphin NetworkManager NetworkManager-tui fuzzel wofi swayidle swaylock swaybg qt6ct brightnessctl pipewire wireplumber emacs-nox neovim entr qemu virt-manager
 
-copr_install_isolated() {
-    local copr_name="$1"
-    shift
-    local packages=("$@")
-
-    if [[ ${#packages[@]} -eq 0 ]]; then
-mkdir -p ~/.config/containers/systemd/        echo "ERROR: No packages specified for copr_install_isolated"
-        return 1
-    fi
-
-    repo_id="copr:copr.fedorainfracloud.org:${copr_name//\//:}"
-
-    echo "Installing ${packages[*]} from COPR $copr_name (isolated)"
-
-    dnf5 -y copr enable "$copr_name"
-    dnf5 -y copr disable "$copr_name"
-    dnf5 -y install --enablerepo="$repo_id" "${packages[@]}"
-
-    echo "Installed ${packages[*]} from $copr_name"
-}
-
-copr_install_isolated "ublue-os/packages" \
-    "ublue-bling" \
-    "ublue-brew" \
-    "ublue-fastfetch" \
-    "ublue-polkit-rules" \
-    "ublue-setup-services" \
-    "uupd"
-
 systemctl enable podman.socket
 systemctl enable NetworkManager.service
 systemctl enable libvirtd.service
