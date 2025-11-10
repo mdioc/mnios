7978cf3d71719745ce36955f14e747ad88d7dc1b
mdioc <oss@znta.io>
2025-11-09 22:16:03 -0500

    switching base image

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,9 +2,4 @@
 
 set -ouex pipefail
 
-dnf5 install -y tmux niri sddm alacritty firefox dolphin NetworkManager NetworkManager-tui fuzzel wofi swayidle swaylock swaybg qt6ct brightnessctl pipewire wireplumber emacs-nox neovim entr qemu virt-manager
-
-systemctl enable podman.socket
-systemctl enable NetworkManager.service
-systemctl enable libvirtd.service
-systemctl enable sddm.service
+dnf5 install -y tmux niri alacritty fuzzel wofi swayidle swaylock swaybg qt6ct brightnessctl emacs-nox neovim entr
